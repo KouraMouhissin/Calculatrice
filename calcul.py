bd4f94314fee2a388e74cf0d8609e385a1d75ee8
aSEMEGA <semegaaliou4@gmail.com>
2023-03-16 16:04:04 +0000

    la modificatio apporter est que les deux lieux et il y'a une classe qui comporte mes methodes dans calcul.py et dans l'autre la classe est instancier puis affiche le resultat de mes methodes effectué dans calcul.py

--- calcul.py
+++ calcul.py
@@ -10,44 +10,53 @@ choix_q=input("Voulez vous Quitter ? (o/n) ")
 		elif choix_q=="n" or choix_q=="N":
 			continue
 """
+class Calculatrice():
 
-def multiplicateur(a,b):
-	print(a," X ", b, " = ",a*b,"\n")
-   
-def addition(a,b):
-	print(a," + ", b, " = ",a+b,"\n")
+	def __init__(self, longeur,largeur):
+		self.longeur = longeur
+		self.largeur = largeur
+	def multiplicateur(self):
+		return  (int (self.longeur) * (int(self.largeur)))
 
-def soustraction(a,b):
-	print(a," - ", b, " = ",a-b,"\n")
+	def addition(self):
+		return  (int (self.longeur) + (int(self.largeur)))
 
-def division(a,b):
-	print(a," / ", b," = ", a/b,"\n")
+	def soustraction(self):
+		return (int(self.longeur) - (int(self.largeur)))
 
-def oui_non():
-	ouvert = True
-	while ouvert:
-		try:
-			a=input("Voulez vous Quitter ? (o/n) ")
+	def division(self):
+		return  (int (self.longeur) / (int(self.largeur)))
 
-			if a=="o" or a=="O":
+	def modulo(self):
+		return  (int (self.longeur) % (int(self.largeur)))
+
+	def oui_non(self):
+		ouvert = True
+		while ouvert:
+			try:
+				a = input("Voulez vous Quitter ? (o/n) ")
+
+				if a == "o" or a == "O":
+					print("\n\n===== Au revoir ... =====")
+					quit()
+				elif a == "n" or a == "N":
+					pass
+				else:
+					print("\n===== Choissez o/O ou n/N ! =====\n")
+					continue
+			except KeyboardInterrupt:
 				print("\n\n===== Au revoir ... =====")
 				quit()
-			elif a=="n" or a=="N":
-				pass
-			else:
-				print("\n===== Choissez o/O ou n/N ! =====\n")
-				continue
-		except KeyboardInterrupt:
-			print("\n\n===== Au revoir ... =====")
-			quit()
+
 
 
 # POUR TESTER LE MODULE SEULEMENT 
-if __name__ == "__main__":
-	print("=== Phase de Test ===\n")
+#if __name__ == "__main__":
+	#print("=== Phase de Test ===\n")
 
-	multiplicateur(5,6)
-	addition(5,6)
-	soustraction(5,6)
-	division(5,6)
-	oui_non()
+	#multiplicateur(5,6)
+	#addition(5,6)
+	#soustraction(5,6)
+	#division(5,6)
+	#modulo(20,8)
+	#oui_non()
--- calculatrice_v2.py
+++ calculatrice_v2.py
@@ -1,139 +1,21 @@
 #coding:utf-8
 
-from calcul import *
+import calcul
 
-print("\n===== Calculatrice v2 =====")
-print("\n 1. Addition\n 2. Soustraction\n 3. Multiplication\n 4. Division\n\n 00. Quitter\n")
+try :
+	a = input("Entrer un nombre >")
+	b = input("Entrer un autre nombre >")
 
-ouvert = True 
-while ouvert:
-	try:
-		choix=input("Choix > ")
-		choix=int(choix)
+	cal = calcul.Calculatrice(int(a), int(b))
 
-		assert 0 <= choix <= 4
+	print(f"l'addition de ces deux est {cal.addition()}")
+	print(f"la soustration de ces deux est {cal.soustraction()}")
+	print(f"la multiplication de ces deux est {cal.multiplicateur()}")
+	print(f"la division de ces deux est {cal.division()}")
+	print(f"le modulo de ces deux nombre est {cal.modulo()}")
+except:
+	print("une erreur c'est produite")
 
-	except AssertionError:
-		print("===== Tapez un chiffre entre 1 et 4 ! =====")
-		continue
-	except ValueError:
-		print("===== Tapez un chiffre ! =====")
-		continue
-	except KeyboardInterrupt:
-		print("\n\n===== Au revoir ... =====")
-		quit()
 
-	if choix==1:
-		ouvert = True
-		while ouvert:
-			try:
-				a = int(input("a = "))
-				break
-			except ValueError:
-				print("===== Entrez un chiffre ! =====")
-				continue
-			except KeyboardInterrupt:
-				print("\n\n===== Au revoir ... =====")
-				quit()
 
-		ouvert = True
-		while ouvert:
-			try:
-				b = int(input("b = "))
-				break
-			except ValueError:
-				print("===== Entrez un chiffre ! =====")
-				continue
-			except KeyboardInterrupt:
-				print("\n\n===== Au revoir ... =====")
-				quit()
 
-		addition( a , b )
-
-	elif choix==2:
-		ouvert = True
-		while ouvert:
-			try:
-				a = int(input("a = "))
-				break
-			except ValueError:
-				print("===== Entrez un chiffre ! =====")
-				continue
-			except KeyboardInterrupt:
-				print("\n\n===== Au revoir ... =====")
-				quit()
-
-		ouvert = True
-		while ouvert:
-			try:
-				b = int(input("b = "))
-				break
-			except ValueError:
-				print("===== Entrez un chiffre ! =====")
-				continue
-			except KeyboardInterrupt:
-				print("\n\n===== Au revoir ... =====")
-				quit()
-
-		soustraction( a , b )
-
-	elif choix==3:
-		ouvert = True
-		while ouvert:
-			try:
-				a = int(input("a = "))
-				break
-			except ValueError:
-				print("===== Entrez un chiffre ! =====")
-				continue
-			except KeyboardInterrupt:
-				print("\n\n===== Au revoir ... =====")
-				quit()
-
-		ouvert = True
-		while ouvert:
-			try:
-				b = int(input("b = "))
-				break
-			except ValueError:
-				print("===== Entrez un chiffre ! =====")
-				continue
-			except KeyboardInterrupt:
-				print("\n\n===== Au revoir ... =====")
-				quit()
-
-		multiplicateur( a , b )
-
-	elif choix==4:
-		ouvert = True
-		while ouvert:
-			try:
-				a = int(input("a = "))
-				break
-			except ValueError:
-				print("===== Entrez un chiffre ! =====")
-				continue
-			except KeyboardInterrupt:
-				print("\n\n===== Au revoir ... =====")
-				quit()
-
-		ouvert = True
-		while ouvert:
-			try:
-				b = int(input("b = "))
-				assert b != 0
-				break
-			except AssertionError:
-				print("===== La division par 0 est Impossible ! =====")
-				continue
-			except ValueError:
-				print("===== Entrez un chiffre ! =====")
-				continue
-			except KeyboardInterrupt:
-				print("\n\n===== Au revoir ... =====")
-				quit()
-
-		division( a , b )
-		
-	elif choix==00: 
-		oui_non()
